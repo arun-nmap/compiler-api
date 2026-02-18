d52db74dd278ad5723e049fd3ee89d1434ce7081
arun <arunkumargudi9@gmail.com>
2026-02-18 18:06:49 +0000

    Fix port to 80 for Azure

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 /* ================================
    Compile and Run C Code
